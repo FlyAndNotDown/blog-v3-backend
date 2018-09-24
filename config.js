93b31af279fa2322736f529a1f8ed617b8329c9f
John Kindem <461425614@qq.com>
2018-09-25 01:17:31 +0800

    [add] 建 npm 库 [add] 配置文件

---
+++ config.js
@@ -0,0 +1,14 @@
+// 导出配置
+module.exports = {
+    // 数据库配置
+    databse: {
+        // 连接配置
+        connection: {
+            url: '134.175.59.165',
+            port: '3306',
+            database: 'blog',
+            username: 'development',
+            password: 'development'
+        }
+    }
+};
