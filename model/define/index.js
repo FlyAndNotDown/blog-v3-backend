39be08ef78857e4edaf4f52b9b5fccd73008978b
John Kindem <461425614@qq.com>
2018-09-27 00:06:31 +0800

    [alter] 修改 Model Index

--- model/define/index.js
+++ model/define/index.js
@@ -5,12 +5,14 @@
 
 const post = require('./post');
 const label = require('./label');
+const admin = require('./admin');
 
 let modelDefineArray = [];
 
 (function() {
     modelDefineArray.push(post);
     modelDefineArray.push(label);
+    modelDefineArray.push(admin);
 })();
 
 module.exports = modelDefineArray;
