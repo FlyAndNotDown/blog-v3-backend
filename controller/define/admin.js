3ed1d9bff602fd45ff578a7abfbea4f646f2fd61
John Kindem <461425614@qq.com>
2018-09-27 17:01:18 +0800

    [add] 添加 get /request/blog/admin 的服务器日志

--- controller/define/admin.js
+++ controller/define/admin.js
@@ -6,15 +6,19 @@
 const controllerConfig = require('../../config/controller');
 const HttpError = require('../../tool/http-error');
 const settings = require('../../settings');
+const Log = require('../../tool/log');
 
 module.exports = {
     url: `${controllerConfig.commonUrlPrefix}/admin`,
     // 获取盐
     get: function(connection, model) {
         return function(req, res) {
+            // 控制台信息
+            Log.log(`get ${controllerConfig.commonUrlPrefix}/admin`);
             // 参数校验
             let username = req.query.username || null;
             if (!username) {
+                Log.log(`response 500`);
                 return settings.devMode ? res.status(500).json(
                     HttpError.paramsError('need param: username')
                 ) : res.status(500).end();
@@ -22,6 +26,7 @@ module.exports = {
             // 查询管理员用户是否存在
             model.admin.count({ username: username }, (err, count) => {
                 if (err) {
+                    Log.log(`response 500`);
                     return settings.devMode ? res.status(500).json(
                         HttpError.dbQueryError('db query error on model: admin')
                     ) : res.status(500).end();
@@ -29,6 +34,7 @@ module.exports = {
                 // 如果管理员用户不存在
                 if (count === 0) {
                     // 返回空盐
+                    Log.log(`response 200`);
                     return res.json({
                         salt: null
                     });
@@ -36,11 +42,13 @@ module.exports = {
                 // 如果存在则查询管理员用户的盐
                 model.admin.one({ username: username }, (err, adminObject) => {
                     if (err) {
+                        Log.log(`response 500`);
                         return settings.devMode ? res.status(500).json(
                             HttpError.dbQueryError('db query error on model: admin')
                         ) : res.status(500).end();
                     }
                     // 返回结果
+                    Log.log(`response 200`);
                     return res.json({
                         salt: adminObject.salt
                     });
