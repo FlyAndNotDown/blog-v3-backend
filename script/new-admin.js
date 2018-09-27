9c1c2fb449cf41259313b73b11d61aac9a81866e
John Kindem <461425614@qq.com>
2018-09-27 11:57:58 +0800

    [add] 添加管理员账户重复校验

--- script/new-admin.js
+++ script/new-admin.js
@@ -22,29 +22,39 @@ const scriptConfig = require('../config/script');
                 Log.error('创建管理员账户失败', '用户名不符合规格');
                 process.exit(0);
             }
-            // TODO 查验数据库看管理员用户名是否重复
-            readlineInterface.question('password: ', (password) => {
-                if (!password.match(scriptConfig.adminRegex.password)) {
-                    Log.error('创建管理员账户失败', '密码不符合规格');
+            // 查看数据库中是否有重复的管理员账户
+            model.admin.count({ username: username }, (err, count) => {
+                if (err) {
+                    Log.error('创建管理员账户失败', err);
                     process.exit(0);
                 }
-                readlineInterface.question('repeat password: ', (passwordRepeat) => {
-                    if (password !== passwordRepeat) {
-                        Log.error('创建管理员账户失败', '两次输入的密码不相同');
+                if (count > 0) {
+                    Log.error('创建管理员账户失败', '管理员账户已经存在');
+                    process.exit(0);
+                }
+                readlineInterface.question('password: ', (password) => {
+                    if (!password.match(scriptConfig.adminRegex.password)) {
+                        Log.error('创建管理员账户失败', '密码不符合规格');
                         process.exit(0);
                     }
-                    let salt = PasswordTool.getSalt();
-                    model.admin.create({
-                        username: username,
-                        password: PasswordTool.encode(password, salt),
-                        salt: salt
-                    }, (err) => {
-                        if (err) {
-                            Log.error('创建管理员账户失败', err);
+                    readlineInterface.question('repeat password: ', (passwordRepeat) => {
+                        if (password !== passwordRepeat) {
+                            Log.error('创建管理员账户失败', '两次输入的密码不相同');
                             process.exit(0);
                         }
-                        Log.log('创建管理员账户成功');
-                        process.exit(0);
+                        let salt = PasswordTool.getSalt();
+                        model.admin.create({
+                            username: username,
+                            password: PasswordTool.encode(password, salt),
+                            salt: salt
+                        }, (err) => {
+                            if (err) {
+                                Log.error('创建管理员账户失败', err);
+                                process.exit(0);
+                            }
+                            Log.log('创建管理员账户成功');
+                            process.exit(0);
+                        });
                     });
                 });
             });
