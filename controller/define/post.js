803ad60b74308e7bb28a5e8a67aea79212652351
John Kindem <461425614@qq.com>
2018-10-07 11:47:04 +0800

    [add] 还是要先把 admin 的搞定

--- controller/define/post.js
+++ controller/define/post.js
@@ -12,19 +12,12 @@ const url = `${controllerConfig.commonUrlPrefix}/post`;
  */
 module.exports = {
     url: url,
-
-    /**
-     * 获取文章
-     * @param {Object} connection 连接对象
-     * @param {Object} model 模型
-     * @returns {Function} http处理函数
-     */
     get: function (connection, model) {
-        /**
-         * @param {Object} request 请求对象
-         * @param {Object} response 响应对象
-         * @returns {*}
-         */
+        return function (request, response) {
+            // TODO
+        }
+    },
+    post: function (connection, model) {
         return function (request, response) {
             // TODO
         }
