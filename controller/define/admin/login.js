92816a689f00bd6e1ea54d8bbfb5736e746692d7
John Kindem <461425614@qq.com>
2018-10-07 14:17:09 +0800

    [add] 添加管理员登录 API

--- controller/define/admin/login.js
+++ controller/define/admin/login.js
@@ -4,6 +4,7 @@
  */
 
 const controllerConfig = require('../../../config/controller');
+const Log = require('../../../tool/log');
 
 const url = `${controllerConfig.commonUrlPrefix}/admin/login`;
 
@@ -14,7 +15,30 @@ module.exports = {
     url: url,
     get: function (connection, model) {
         return function (request, response) {
-            // TODO
+            // 获取参数
+            const query = request.query || {};
+            const username = query.username || {};
+
+            // 查询 username 是否存在
+            model.admin.one({
+                username: username
+            }, function (error, object) {
+                // 如果查询出错
+                if (error) {
+                    Log.error('数据库查询错误', error);
+                    return response.status(500).end();
+                }
+                // 如果存在
+                if (object) {
+                    return response.json({
+                        salt: object.salt
+                    });
+                } else {
+                    return response.json({
+                        salt: null
+                    });
+                }
+            });
         }
     }
 };
