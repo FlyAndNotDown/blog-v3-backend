91c9cb4e18ef3ca0d949c1e23251ea1257a01a7b
John Kindem <461425614@qq.com>
2018-10-08 20:20:05 +0800

    [refactor] 一点重构

--- model/define.js
+++ model/define.js
@@ -6,88 +6,83 @@
 const Sequelize = require('sequelize');
 
 /**
- * 获取 Post 模型
- * @param {Object} model 模型对象
- * @returns {Model|*|void} 文章模型
+ * 文章模型定义
  */
-function getPostModel(model) {
-    return model.define('post', {
-        id: {
-            type: Sequelize.BIGINT,
-            unique: true,
-            autoIncrement: true
-        },
-        title: {
-            type: Sequelize.STRING(100)
-        },
-        description: {
-            type: Sequelize.STRING(1000)
-        },
-        body: {
-            type: Sequelize.STRING(1000)
-        },
-        date: {
-            type: Sequelize.DATEONLY
-        }
-    });
-}
+const postModel = {
+    id: {
+        type: Sequelize.BIGINT,
+        unique: true,
+        autoIncrement: true
+    },
+    title: {
+        type: Sequelize.STRING(100)
+    },
+    description: {
+        type: Sequelize.STRING(1000)
+    },
+    body: {
+        type: Sequelize.STRING(1000)
+    },
+    date: {
+        type: Sequelize.DATEONLY
+    }
+};
 
 /**
- * 获取标签模型
- * @param {Object} model 模型对象
- * @returns {Model|*|void} 标签模型
+ * 标签模型定义
  */
-function getLabelModel(model) {
-    return model.define('label', {
-        id: {
-            type: Sequelize.BIGINT,
-            unique: true,
-            autoIncrement: true
-        },
-        name: {
-            type: Sequelize.STRING(20)
-        }
-    });
-}
+const labelModel = {
+    id: {
+        type: Sequelize.BIGINT,
+        unique: true,
+        autoIncrement: true
+    },
+    name: {
+        type: Sequelize.STRING(20)
+    }
+};
 
 /**
- * 获取管理员模型
- * @param {Object} model 模型对象
- * @returns {Model|*|void} 管理员模型
+ * 管理员模型定义
  */
-function getAdminModel(model) {
-    return model.define('admin', {
-        id: {
-            type: Sequelize.BIGINT,
-            unique: true,
-            autoIncrement: true
-        },
-        name: {
-            type: Sequelize.STRING(20)
-        },
-        username: {
-            type: Sequelize.STRING(20)
-        },
-        password: {
-            type: Sequelize.STRING(64)
-        },
-        salt: {
-            type: Sequelize.STRING(12)
-        },
-        phone: {
-            type: Sequelize.STRING(11)
-        }
-    });
-}
+const adminModel = {
+    id: {
+        type: Sequelize.BIGINT,
+        unique: true,
+        autoIncrement: true
+    },
+    name: {
+        type: Sequelize.STRING(20)
+    },
+    username: {
+        type: Sequelize.STRING(20)
+    },
+    password: {
+        type: Sequelize.STRING(64)
+    },
+    salt: {
+        type: Sequelize.STRING(12)
+    },
+    phone: {
+        type: Sequelize.STRING(11)
+    }
+};
+
+const postLabelRelation = {
+    type: 'm2m',
+    owner: ['post', 'label']
+};
 
 /**
  * 导出定义列表
  */
 module.exports = {
     model: [
-        getPostModel,
-        getLabelModel,
-        getAdminModel
+        postModel,
+        labelModel,
+        adminModel
     ],
-    relation: []
+    relation: [
+        postLabelRelation
+    ]
 };
