56e3a3c49f5874c3e0182fdccce15b0ca90af9ac
John Kindem <461425614@qq.com>
2018-10-09 01:12:57 +0800

    [fix] 去除 Seq 乱七八糟的控制器输出

--- config.js
+++ config.js
@@ -34,7 +34,9 @@ const modelConfig = mainConfig.devMode ? {
                 min: 0,
                 acquire: 30000,
                 idle: 10000
-            }
+            },
+            logging: false,
+            operatorsAliases: false
         }
     }
 } : {};
