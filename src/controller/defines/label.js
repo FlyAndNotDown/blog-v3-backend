d2e4df67c367b20deacbbfb51a177111b5722d0e
John Kindem <461425614@qq.com>
2018-10-11 13:10:56 +0800

    [add] 添加获取所有标签 API

--- src/controller/defines/label.js
+++ src/controller/defines/label.js
@@ -16,7 +16,48 @@ export default {
     get: (db, models) => {
         return async (ctx, next) => {
             await next();
-            // TODO
+
+            // 获取类型参数
+            const query = ctx.request.query || {};
+            const type = query.type || null;
+
+            // 参数校验
+            if (!type) {
+                Log.error('status 400', `type: ${type}`);
+                ctx.response.status = 400;
+                return null;
+            }
+
+            // 根据不同类型进行不同处理
+            switch (type) {
+                case 'all':
+                    // 获取所有标签
+                    let labels = null;
+                    try {
+                        labels = models.label.findAll();
+                    } catch (e) {
+                        Log.error('status 500', e);
+                        ctx.response.status = 500;
+                        return null;
+                    }
+                    // 返回结果
+                    let result = [];
+                    labels.forEach(label => result.push({
+                        id: label.id,
+                        name: label.name
+                    }));
+                    ctx.response.body = {
+                        labels: result
+                    };
+                    return null;
+                case 'some':
+                    // TODO
+                    return null;
+                default:
+                    Log.error('status 400', `type ${type}`);
+                    ctx.response.status = 400;
+                    return null;
+            }
         }
     }
 }
