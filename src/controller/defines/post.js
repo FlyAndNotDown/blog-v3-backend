bcb58130e82b03bfc0c9408a7a6058eaab0e5559
John Kindem <461425614@qq.com>
2018-10-11 11:39:37 +0800

    [add] 部分 post 逻辑

--- src/controller/defines/post.js
+++ src/controller/defines/post.js
@@ -4,6 +4,11 @@
  */
 
 import controllerConfig from '../../configs/controller';
+import regexConfig from '../../configs/regex';
+import { Log } from "../../tool/log";
+import label from "../../model/defines/model/label";
+
+const postRegex = regexConfig.post;
 
 /**
  * /post 控制器
@@ -17,12 +22,79 @@ export default {
             // 校验管理员登录情况
             if (!ctx.session.adminLogin) {
                 // 如果没有登录
+                Log.error('status 401', `session.adminLogin: ${ctx.session.adminLogin}`);
                 ctx.response.status = 401;
                 return null;
             }
 
             // 如果已经登录了，则允许新建文章
             // 获取参数
+            const requestBody = ctx.request.body || {};
+            const title = requestBody.title || null;
+            const body = requestBody.body || null;
+            const description = requestBody.description || null;
+            const labels = requestBody.labels || null;
+
+            // 参数校验
+            if (!title || !title.match(postRegex.title)) {
+                Log.error('status 400', `title: ${title}`);
+                ctx.response.status = 400;
+                return null;
+            }
+            if (!description || !description.match(postRegex.description)) {
+                Log.error('status 400', `description: ${description}`);
+                ctx.response.status = 400;
+                return null;
+            }
+
+            // TODO xss 过滤器
+
+            // 查询所有标签
+            let dbLabelObjects;
+            let dbLabelNames = [];
+            try {
+                dbLabelObjects = await models.label.findAll();
+                dbLabelObjects.forEach(object => dbLabelNames.push(object.name));
+            } catch (e) {
+                Log.error('status 500', e);
+                ctx.response.status = 500;
+                return null;
+            }
+
+            // 看传过来的标签是否都在这个列表中
+            labels.forEach(label => {
+                let find = false;
+                for (let dbLabelName in dbLabelNames) {
+                    if (dbLabelName === label) {
+                        find = true;
+                        break;
+                    }
+                }
+                // 如果不在，则报错
+                if (!find) {
+                    Log.error('status 400', `label ${label} not found in database`);
+                    ctx.response.status = 400;
+                    return null;
+                }
+            });
+
+            // 存入数据库
+            // 先存基础对象
+            let newPost;
+            try {
+                newPost = await models.post.create({
+                    title: title,
+                    description: description,
+                    body: body
+                });
+            } catch (e) {
+                Log.error('status 500', e);
+                ctx.response.status = 400;
+                return null;
+            }
+
+            console.log(newPost);
+
             // TODO
         }
     }
