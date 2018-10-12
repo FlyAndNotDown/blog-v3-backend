7e68fbfc5574cc21647fd150795788ef3c1e1d9a
John Kindem <461425614@qq.com>
2018-10-13 01:34:30 +0800

    [add] 睡觉，明天写

--- src/controller/defines/index.js
+++ src/controller/defines/index.js
@@ -8,9 +8,12 @@
 import controllerConfig from '../../configs/controller';
 import regexConfig from '../../configs/regex';
 import { Log } from '../../tool/log';
+import Sequelize from 'sequelize';
 
 const normalRegex = regexConfig.normal;
 const indexGetMaxAmount = controllerConfig.index.get.maxAmount;
+
+const Op = Sequelize.Op;
 
 /**
  * ${commonUrlPrefix}/index 控制器
@@ -43,15 +46,72 @@ export default {
 
             // 看 amount 是否超过了最大设定
             if (amount > indexGetMaxAmount) {
-                Log.error(`status 400`, `amount is too big, amount: ${amount}`);
+                Log.error('status 400', `amount is too big, amount: ${amount}`);
                 return ctx.response.status = 400;
             }
 
             let postList;
-            let emotinList;
+            let emotionList;
 
-            // 发起查询
-            // TODO
+            // 发起文章查询
+            try {
+                if (postStartId === 0) {
+                    // 如果 startId == 0, 则说明是第一页
+                    postList = await models.post.findAll({
+                        limit: amount,
+                        order: [
+                            ['id', 'DESC']
+                        ]
+                    });
+                } else {
+                    // 如果 startId != 0，则说明不是第一页
+                    postList = await models.post.findAll({
+                        limit: amount,
+                        where: {
+                            id: {
+                                [Op.lt]: postStartId
+                            }
+                        },
+                        order: [
+                            ['id', 'DESC']
+                        ]
+                    });
+                }
+            } catch(e) {
+                Log.error('status 500', e);
+                return ctx.response.status = 500;
+            }
+
+            // 发起说说查询
+            try {
+                if (emotionStartId === 0) {
+                    // 如果 startId == 0, 则说明是第一页
+                    emotionList = await models.emotion.findAll({
+                        limit: amount,
+                        order: [
+                            ['id', 'DESC']
+                        ]
+                    });
+                } else {
+                    // 如果 startId != 0，则说明不是第一页
+                    emotionList = await models.emotion.findAll({
+                        limit: amount,
+                        where: {
+                            id: {
+                                [Op.lt]: emotionStartId
+                            }
+                        },
+                        order: [
+                            ['id', 'DESC']
+                        ]
+                    });
+                }
+            } catch(e) {
+                Log.error('status 500', e);
+                return ctx.response.status = 500;
+            }
+
+            // TODO 组合成主页需要的格式，明天再写了，这傻逼室友又在烦人了
         };
     }
 }
