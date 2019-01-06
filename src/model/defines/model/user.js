7c67c369c9cdebc4dc458b9087873a9fe593a1d1
John Kindem <461425614@qq.com>
2019-01-06 15:46:10 +0800

    [add] add a new Model

---
+++ src/model/defines/model/user.js
@@ -0,0 +1,35 @@
+/**
+ * /model/defines/model/user.js
+ * @author John Kindem
+ * @description model description file of 'user'
+ * @version v1.0
+ */
+
+import Sequelize from 'sequelize';
+
+/**
+ * model define of 'user'
+ */
+export default {
+    name: 'user',
+    description: {
+        id: {
+            type: Sequelize.BIGINT,
+            unique: true,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        type: {
+            type: Sequelize.STRING(10)
+        },
+        key: {
+            type: Sequelize.STRING(100)
+        },
+        nickname: {
+            type: Sequelize.STRING(100)
+        },
+        avatar: {
+            type: Sequelize.STRING(200)
+        }
+    }
+};
