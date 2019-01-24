c6237c5c96cdd9db2ac09985dcfe8ccc234378f7
John Kindem <461425614@qq.com>
2019-01-24 21:34:19 +0800

    [fix] fix a bug

--- src/controller/define.js
+++ src/controller/define.js
@@ -7,10 +7,14 @@ import adminLoginController from './defines/admin/login';
 import testController from './defines/test';
 import postController from './defines/post';
 import labelController from './defines/label';
+import userLocalController from './defines/user/local';
+import userLoginController from './defines/user/login';
 
 export default [
     testController,
     adminLoginController,
     postController,
-    labelController
+    labelController,
+    userLocalController,
+    userLoginController
 ];
