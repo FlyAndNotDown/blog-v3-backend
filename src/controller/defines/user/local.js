7a8eedbd6d4a4456308c1f4f8ac1ed36940e03d5
John Kindem <461425614@qq.com>
2019-01-27 15:04:19 +0800

    ✨ some refactor about //user/local & //user/login controllers

--- src/controller/defines/user/local.js
+++ src/controller/defines/user/local.js
@@ -14,11 +14,14 @@ const userRegex = regexConfig.user;
 
 /**
  * /${commonUrlPrefix}/user/local controller
- * @description {get} get local user salt of password
+ * @description {get} get local user's salt of password
  * * @param {'salt'} type type of get request
- * * @param {string} username username of local user
+ * * @param {string} email email of local user
+ * @description {get} get local user's account actived status
+ * * @param {'salt'} type type of get request
+ * * @param {string} email email of local user
  * @description {post} register a new local user account
- * * @param {string} username username of user
+ * * @param {string} email email of user
  * * @param {string} nickname nickname of user
  * * @param {string} salt salt of password
  * * @param {string} password sha256 hash value of password
@@ -29,139 +32,14 @@ export default {
         return async (context, next) => {
             await next();
 
-            // get params
-            const query = context.request.query || {};
-            const type = query.type || null;
-
-            // check the params
-            if (!type) {
-                Log.error('status 400', `type: ${type}`);
-                return context.response.status = 400;
-            }
-
-            // do different thing when get different type
-            switch (type) {
-                case 'salt':
-                    // get params
-                    const username = query.username || null;
-
-                    // check the params
-                    if (!username || !username.match(userRegex.username)) {
-                        Log.error('status 400', `type: ${type}`);
-                        return context.response.status = 400;
-                    }
-
-                    // // query the database
-                    // let count = 1;
-                    // try {
-                    //     count = await models.user.count({
-                    //         username: username
-                    //     });
-                    // } catch (e) {
-                    //     Log.error('status 500', e);
-                    //     return context.response.status = 500;
-                    // }
-                    //
-                    // // if username have exist
-                    // if (count > 0) {
-                    //     return context.response.body = {
-                    //         salt: null
-                    //     };
-                    // }
-
-                    // query the database
-                    let user;
-                    try {
-                        user = await models.user.findOne({
-                            where: {
-                                username: username
-                            }
-                        });
-                    } catch (e) {
-                        Log.error('status 500', e);
-                        return context.response.status = 500;
-                    }
-
-                    // if not found
-                    if (!user) {
-                        return context.response.body = {
-                            salt: null
-                        };
-                    }
-
-                    // if found
-                    return context.response.body = {
-                        salt: user.salt || null
-                    };
-                default:
-                    Log.error('status 400', `type: ${type}`);
-                    return context.response.status = 400;
-            }
+            // TODO
         };
     },
     post: (db, models) => {
-        return async (ctx, next) => {
+        return async (context, next) => {
             await next();
 
-            // get params
-            const query = ctx.request.query || {};
-            const username = query.username || null;
-            const nickname = query.nickname || null;
-            const salt = query.salt || null;
-            const password = query.password || null;
-
-            // check the params
-            if (!username || !username.match(userRegex.username)) {
-                Log.error('status 400', `username: ${username}`);
-                return ctx.response.status = 400;
-            }
-            if (!nickname || !nickname.match(userRegex.nickname)) {
-                Log.error('status 400', `nickname: ${nickname}`);
-                return ctx.response.status = 400;
-            }
-            if (!salt || !salt.match(userRegex.salt)) {
-                Log.error('status 400', `salt: ${salt}`);
-                return ctx.response.status = 400;
-            }
-            if (!password || !password.match(userRegex.passwordHash)) {
-                Log.error('status 400', `password: ${password}`);
-                return ctx.response.status = 400;
-            }
-
-            // judge if the username have been used
-            let count = 1;
-            try {
-                count = await models.user.count({
-                    username: username
-                });
-            } catch (e) {
-                Log.error('status 500', e);
-                return ctx.response.status = 500;
-            }
-
-            if (count > 0) {
-                return ctx.response.body = {
-                    success: false,
-                    reason: '用户名已经被使用'
-                };
-            }
-
-            // save all the info to the database
-            try {
-                await models.user.create({
-                    username: username,
-                    password: password,
-                    nickname: nickname,
-                    salt: salt
-                });
-            } catch (e) {
-                Log.error('status 500', e);
-                return ctx.response.status = 500;
-            }
-
-            return ctx.response.body = {
-                success: true
-            };
+            // TODO
         };
     }
 };
---
+++ src/controller/defines/user/login-old.js
@@ -0,0 +1,152 @@
+/**
+ * /controller/defines/user/login.js
+ * @author John Kindem
+ * @description /${commonUrlPrefix}/user/login 控制器
+ * @version v1.0
+ */
+
+import controllerConfig from '../../../configs/controller';
+import regexConfig from '../../../configs/regex';
+import { Log } from '../../../tool/log';
+
+const { commonUrlPrefix } = controllerConfig;
+const userRegex = regexConfig.user;
+
+/**
+ * /${commonUrlPrefix}/user/login 控制器
+ * @description {get} get login user info
+ * * @param {'info'} type type of get
+ * @description {post} user login
+ * * @param {'local'} userType type of user
+ * * @param {string} username username
+ * * @param {string} password sha256 hash of password
+ * @description {delete} logout
+ */
+export default {
+    url: `${commonUrlPrefix}/user/login`,
+    get: (db, models) => {
+        return async (ctx, next) => {
+            await next();
+
+            // get type param
+            const query = ctx.request.query || {};
+            const type = query.type || null;
+
+            // check the param
+            if (!type) {
+                Log.error('status 400', `type: ${type}`);
+                return ctx.response.status = 400;
+            }
+
+            // do something due type
+            switch (type) {
+                case 'info':
+                    // if get type is 'info', get user info from session
+                    return ctx.response.body = {
+                        login: !!ctx.session.userLogin,
+                        info: ctx.session.userInfo || null
+                    };
+                default:
+                    Log.error('status 400', `type: ${type}`);
+                    return ctx.response.status = 400;
+            }
+        };
+    },
+    post: (db, models) => {
+        return async (ctx, next) => {
+            await next();
+
+            // get params
+            const query = ctx.request.query || {};
+            const userType = query.userType || null;
+
+            // check the params
+            if (!userType) {
+                Log.error('status 400', `userType: ${userType}`);
+                return ctx.response.status = 400;
+            }
+
+            // different deal when different userType
+            switch (userType) {
+                case 'local':
+                    // get the params
+                    const body = ctx.request.body || {};
+                    const username = body.username || null;
+                    const password = body.password || null;
+
+                    // check the params
+                    if (!username || !username.match(userRegex.username)) {
+                        Log.error('status 400', `username: ${username}`);
+                        return ctx.response.status = 400;
+                    }
+                    if (!password || !password.match(userRegex.passwordHash)) {
+                        Log.error('status 400', `password: ${password}`);
+                        return ctx.response.status = 400;
+                    }
+
+                    // query the database to get user object
+                    let user;
+                    try {
+                        user = await models.user.findOne({
+                            where: {
+                                username: username
+                            }
+                        });
+                    } catch (e) {
+                        Log.error('status 500', e);
+                        return ctx.response.status = 500;
+                    }
+
+                    // if not found
+                    if (!user) {
+                        return ctx.response.body = {
+                            success: false
+                        };
+                    }
+
+                    // if found, check the password
+                    if (user.password !== password) {
+                        return ctx.response.body = {
+                            success: false
+                        };
+                    }
+
+                    // if check success, save the login status to the session
+                    ctx.session.userLogin = true;
+                    ctx.session.userInfo = {
+                        id: user.id,
+                        type: user.type,
+                        key: user.key,
+                        nickname: user.nickname,
+                        avatar: user.avatar,
+                        username: user.username
+                    };
+
+                    // return the result
+                    return ctx.response.body = {
+                        success: true
+                    };
+                case 'github':
+                    // TODO
+                    return null;
+                case 'qq':
+                    // TODO
+                    return null;
+                default:
+                    Log.error('status 400', `userType: ${userType}`);
+                    break;
+            }
+        };
+    },
+    delete: (db, models) => {
+        return async (ctx, next) => {
+            await next();
+
+            // delete the session info
+            ctx.session.userLogin = false;
+            ctx.session.userInfo = null;
+
+            return;
+        };
+    }
+};
