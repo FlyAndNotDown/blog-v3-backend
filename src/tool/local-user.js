73a553af120d12dfd72184e99207ccfb3265a15e
John Kindem <461425614@qq.com>
2019-02-02 12:02:15 +0800

    🎉 add a tool class

--- src/tool/local-user.js
+++ src/tool/local-user.js
@@ -5,57 +5,51 @@
  * @version v1.0
  */
 
- import sha256 from 'js-sha256';
- import nodeMailer from 'nodemailer';
- import { connection, info, activeCallback } from '../configs/mail';
+import sha256 from 'js-sha256';
+import nodeMailer from 'nodemailer';
+import { connection, info, activeCallback } from '../configs/mail';
 
- /**
-  * User Tool
-  * @constructor
-  */
- export class LocalUserTool {
+/**
+ * User Tool
+ * @constructor
+ */
+export class LocalUserTool {
 
     /**
-     * get public key function
-     * @returns {string} public key
+     * get active code letter vertex
+     * @returns {[string]} active code letter vertex
      */
-    static getPublicKey() {
-        return 'Blog of Kindem';
-    }
-    
-    /**
-     * get active code function
-     * @param {string} email email of user
-     * @param {number} id id of user
-     * @returns {string} sha256 hash active code
-     * @static
-     */
-    static getActiveCode(email, id) {
-        return sha256(sha256(`${email} ${id} ${LocalUserTool.getPublicKey()}`));
+    static __getActiveCodeLetterVertex() {
+        return [
+            'A', 'B', 'C', 'D', 'E', 'F',
+            'G', 'H', 'I', 'J', 'K', 'L',
+            'M', 'N', 'O', 'P', 'Q', 'R',
+            'S', 'T', 'U', 'V', 'W', 'X',
+            'Y', 'Z'
+        ];
     }
 
     /**
-     * send active email to user's email
-     * @static
+     * get active code length
+     * @returns active code length
      */
-    static sendActiveMail(email) {
-        // create connection
-        let transport = nodeMailer.createTransport(connection);
-        
-        // get the active link
-        let activeLink = `${activeCallback}?type=active&email=${email}`;
-
-        // send a active email
-        transport.sendMail({
-            from: info.sender,
-            to: email,
-            subject: info.subject,
-            text: `${info.publicText} ${activeLink}`,
-            html: `${info.publicHtml} <p><a href="${activeLink}">激活链接</a></p>`
-        });
-
-        // close the connection
-        transport.close();
+    static __getActiveCodeLength() {
+        return 6;
     }
 
- };
+    /**
+     * get random active code
+     * @returns a random active code
+     */
+    static getRandomActiveCode() {
+        let activeCode = '';
+        let activeCodeLetterVertex = LocalUserTool.__getActiveCodeLetterVertex();
+
+        for (let i = 0; i < LocalUserTool.__getActiveCodeLength; i++) {
+            activeCode += activeCodeLetterVertex[Math.floor(Math.random() * activeCodeLetterVertex.length)];
+        }
+
+        return activeCode;
+    }
+
+};
