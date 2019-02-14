4a0dc0af61213583e042d0140b7cbddf20a29f60
John Kindem <461425614@qq.com>
2019-02-14 17:57:35 +0800

    🎉 add a new API

--- src/controller/defines/comment.js
+++ src/controller/defines/comment.js
@@ -16,6 +16,18 @@ const normalRegex = regexConfig.normal;
  * @description {get} get all comments in a post
  * @param {'post'} type get type
  * @param {string} postId post id
+ * 
+ * @description {post} publish a new comment
+ * @param {'comment'} type post type
+ * @param {string} postId post id
+ * @param {string} body comment body
+ * 
+ * @description {post} publish a new reply
+ * @param {'reply'} type post type
+ * @param {string} postId post id
+ * @param {string} parentId parent comment id
+ * @param {string} mentionId mention user id
+ * @param {string} body comment body
  */
 export default {
     url: `${controllerConfig.commonUrlPrefix}/comment`,
@@ -123,6 +135,120 @@ export default {
                     return context.response.body = {
                         comments: result
                     };
+                default:
+                    Log.error('status 400', `type: ${type}`);
+                    return context.response.status = 400;
+            }
+        };
+    },
+    post: (database, models) => {
+        return async (context, next) => {
+            await next();
+
+            // get params
+            const body = context.request.body || {};
+            const type = body.type || null;
+
+            // param check
+            if (!type) {
+                Log.error('status 400', `type: ${type}`);
+                return context.response.status = 400;
+            }
+
+            // do different thing when get different 'type' param
+            switch (type) {
+                case 'comment':
+                    // get params
+                    const postId = commentBody.postId || null;
+                    const commentBody = commentBody.body || null;
+
+                    // check params
+                    if (!postId || !postId.match(normalRegex.naturalNumber)) {
+                        Log.error('status 400', `postId: ${postId}`);
+                        return context.response.status = 400;
+                    }
+                    if (!commentBody) {
+                        Log.error('status 400', `body: ${commentBody}`);
+                        return context.response.status = 400;
+                    }
+
+                    // query if post exist
+                    let exist;
+                    try {
+                        exist = await models.post.exist({
+                            where: {
+                                id: postId
+                            }
+                        });
+                    } catch (e) {
+                        Log.error('status 500', e);
+                        return context.response.status = 500;
+                    }
+
+                    // if not exist
+                    if (!exist) {
+                        Log.error('status 400', `postId: ${postId}`);
+                        return context.response.status = 400;
+                    }
+
+                    // judge if user login
+                    if (!context.session.userLogin || !context.session.userInfo) {
+                        Log.error('status 400', `userLogin: false`);
+                        return context.response.status = 400;
+                    }
+
+                    // get creator user info
+                    let creator;
+                    try {
+                        creator = await models.user.findOne({
+                            where: {
+                                id: context.session.userInfo.id
+                            }
+                        });
+                    } catch (e) {
+                        Log.error('status 500', e);
+                        return context.response.status = 500;
+                    }
+
+                    // if get no user
+                    if (!creator) {
+                        Log.error('status 400', `fake user info in session`);
+                        return context.response.status = 400;
+                    }
+
+                    // create new comment
+                    let comment;
+                    try {
+                        comment = await models.comment.create({
+                            body: commentBody,
+                            isChild: false
+                        });
+                    } catch (e) {
+                        Log.error('status 500', e);
+                        return context.response.status = 500;
+                    }
+
+                    // if create failed
+                    if (!comment) {
+                        Log.error('status 500', `create comment failed`);
+                        return context.response.status = 400;
+                    }
+
+                    // create relation
+                    try {
+                        await creator.addComment(comment);
+                        await post.addComment(comment);                        
+                    } catch (e) {
+                        Log.error('status 500', e);
+                        return context.response.status = 500;
+                    }
+
+                    // return result
+                    return context.response.body = {
+                        success: true
+                    };
+                case 'reply':
+                    return null;
                 default:
                     Log.error('status 400', `type: ${type}`);
                     return context.response.status = 400;
