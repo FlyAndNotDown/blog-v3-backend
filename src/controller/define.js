f139a11e1395f762464c5bb89b3d30664dd45125
John Kindem <461425614@qq.com>
2019-02-17 17:21:18 +0800

    🛠 fix a bug

--- src/controller/define.js
+++ src/controller/define.js
@@ -9,6 +9,7 @@ import postController from './defines/post';
 import labelController from './defines/label';
 import commentController from './defines/comment';
 import messageController from './defines/message';
+import friendController from './defines/friend';
 import userLocalController from './defines/user/local';
 import userLoginController from './defines/user/login';
 
@@ -19,6 +20,7 @@ export default [
     labelController,
     commentController,
     messageController,
+    friendController,
     userLocalController,
     userLoginController
 ];
